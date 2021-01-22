82b7fb6bbf1fc82d1d10ba4bf3751c5989fc646f
vhaegar1526 <dibya.secret@gmail.com>
2021-01-22 22:31:58 +0530

    Authrntication API added.

--- postgres-node/server.js
+++ postgres-node/server.js
@@ -31,54 +31,76 @@ const PORT = 3000;
 app.listen(PORT);
 console.log(`running on http://localhost:${PORT}`);
 
-const users = [];
-
 init();
 
 async function init() {
-  //   *************************************SERVES LOGIN FUNCTIONALITY.*****************************************************************
+  // ******************************************* GETS DATA FROM postgreSQL DB **************************************************
+  async function GET_psql(query) {
+    try {
+      const client = await pool.connect();
+      const [result] = await Promise.all([client.query(`${query}`)]);
+      return result.rows;
+    } finally {
+      console.log("Error ********* Fetching from postgreSQL");
+    }
+  }
+
+  // ******************************************** POSTS DATA INTO postgreSQL DB **************************************************
+  async function POST_psql(query) {
+    try {
+      const client = await pool.connect();
+      await Promise.all([client.query(`${query}`)]);
+    } catch {
+      console.log(`Error ********* Failed posting into postgreSQL`);
+    }
+  }
+
+  //   *************************************SERVES REGISTRATION FUNCTIONALITY.*****************************************************************
 
   app.post("/user/register", async (req, res) => {
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const user = { username: req.body.username, password: hashedPassword };
-      users.push(user);
+      POST_psql(`INSERT INTO users (username, loginpassword, fullname, phoneno, usertype) 
+        VALUES ('${req.body.username}', '${hashedPassword}', '${req.body.fullname}', '${req.body.phoneno}', ${req.body.usertype});`);
       res.status(201).send();
-    } finally {
+    } catch {
       res.status(500).send();
     }
-  });
-
-  //   *************************************SERVES AUTHENTICATION.*****************************************************************
-
-  // See Registered Users.
-  app.get("/user/register", (req, res) => {
-    res.json(users).end();
   });
 
   //   *************************************SERVES LOGIN FUNCTIONALITY.*****************************************************************
 
   app.post("/user/login", async (req, res) => {
-    const user = users.find((user) => user.username === req.body.username);
+    //Look for username in database, if found fetch password and user_id.
+    let user = await GET_psql(
+      `SELECT user_id, loginPassword, userType FROM users WHERE username='${req.body.username}'`
+    );
+    user = user[0];
+
     if (user === null) {
-      return res.status(400).send("Cannot find user!!");
+      return res.status(400).send("User not registered!!");
     }
+
     try {
-      if (await bcrypt.compare(req.body.password, user.password)) {
-        const userTokenRaw = { username: user.username }; // COnfigure payload.
+      if (await bcrypt.compare(req.body.password, user.loginpassword)) {
+        const userTokenRaw = {
+          // Payload configuration.
+          userId: user.user_id,
+          userType: user.usertype,
+        };
         const JWtoken = jwt.sign(userTokenRaw, process.env.ACCESS_TOKEN_SECRET);
 
         // res.send("Success!, Creating Token...");
         res.json({ accessToken: JWtoken }).end("Token Created Successfully!");
       } else {
-        res.send("Access Denied!!");
+        res.send("Access Denied, wrong password ^_^ ");
       }
     } finally {
       res.status(500).send();
     }
   });
 
-  //************* */
+  //******************************* SERVICES WITH AUTHENTICITY CHECK******************* */
 
   app.get(
     "/compliment",
@@ -86,8 +108,9 @@ async function init() {
       const authHeader = req.headers["authorization"];
       const token = authHeader && authHeader.split(" ")[1];
 
-      if (token === null) res.send("Token Missing, access denied").end();
-
+      if (token === null) {
+        res.send("Token Missing, access denied").end();
+      }
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
@@ -99,39 +122,29 @@ async function init() {
     }
   );
 
-  //   *************************************SERVES EMAIL DETAILS.*****************************************************************
+  //   ************************************ Testbench-1.*****************************************************************
   app.get("/get", async (req, res) => {
-    const client = await pool.connect();
-    const [result] = await Promise.all([
-      client.query(
-        `SELECT fullName, email_id, phoneno, timesent, contentfile, emailstatus 
-        FROM users NATURAL INNER JOIN emailinfo NATURAL INNER JOIN emailStored
-        WHERE user_id=${req.query.search};`
-      ),
-    ]);
+    let result = await GET_psql(
+      `SELECT email_id FROM users WHERE user_id='${req.query.search}'`
+    );
+    result = result[0];
 
     res
       .json({
-        result: result.rows || {},
+        loginPassword: result.loginpassword || "",
+        userType: result.usertype || "",
       })
       .end();
   });
-  // **************************************SERVES ADMIN VERIFICATION*******************************************************************
+  // *************************************** Testbench-2.******************************************************************
   app.get("/get/isAdmin", async (req, res) => {
-    const client = await pool.connect();
     const queryFor = req.query.search;
-    console.log(
-      `SELECT usertype FROM users WHERE username='${queryFor}' OR fullname='${queryFor}'`
+    const result = await GET_psql(
+      `SELECT usertype FROM users WHERE username='${queryFor}' OR fullname='${queryFor}';`
     );
-    const [userType] = await Promise.all([
-      client.query(
-        `SELECT usertype FROM users WHERE username='${queryFor}' OR fullname='${queryFor}';`
-      ),
-    ]);
-    console.log(userType.rows[0]);
     res
       .json({
-        isAdmin: userType.rows[0] || {},
+        isAdmin: result[0].usertype || {},
       })
       .end();
   });
